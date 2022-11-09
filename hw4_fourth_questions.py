f3c29598e9244eada3282ea666d8c4acd1f76c44
giovannachaves <gchaves@minerva.kgi.edu>
2022-11-09 15:07:13 +0100

    Fixing a small mistake made in the for loop to print results.

--- hw4_fourth_questions.py
+++ hw4_fourth_questions.py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[5]:
-
 
 # Use the data in covid.csv for this exercise
 #
@@ -12,18 +7,11 @@
 # for those countries that have more than 500, 1000 and 5000
 # active cases respectively.
 # Follow DRY principles in order to complete this exercise.
-#
-#
-# #
 
 import numpy as np
 import pandas as pd
 import os
 path = os.getcwd()
-
-
-# In[220]:
-
 
 df = pd.read_csv(path + "/covid.csv", sep = ",")
 df.head(10)
@@ -41,13 +29,6 @@ for c in cases:
 
 for i in range(len(country_list)):
     print('This is the list of countries with active number of cases greater than '+ str(cases[i]) + ': '
-          + str(country_list[i][:-2]) + '. The average number of deaths among those countries was '
+          + str(country_list[i]) + '. The average number of deaths among those countries was '
           +str(average_list[i][0])+' and the average number of confirmed cases among those countries was '
          +str(average_list[i][1])+'.')
-
-
-# In[ ]:
-
-
-
-
